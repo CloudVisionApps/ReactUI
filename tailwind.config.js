db8dd529e9751e6688ccb8d475c05f43ad6277db
Bozhidar <bobicloudvision@gmail.com>
2026-02-08 22:59:06 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,5 @@ module.exports = {
   content: [
     path.join(__dirname, './src/**/*.{js,jsx,ts,tsx}'),
     path.join(__dirname, './preview/**/*.{js,jsx,ts,tsx}'),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: []
+  ]
 };
